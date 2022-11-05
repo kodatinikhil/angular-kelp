f47b34a36eaef4934f3778cefa1d5a96679e53ea
kodatinikhil <75851891+kodatinikhil@users.noreply.github.com>
2022-11-05 23:52:19 +0530

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,25 +5,83 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
+
+
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  user = Array<{name: string, url: string, message: string}>;
+  //user=[];
+  
   constructor(){
-
+    
+ 
   }
+  
   ngOnInit(){
+    let that=this;
+
+    allStorage();
+    //console.log(user["adcds fdsvfdvd"]);
+      function allStorage() {
+
+        var archive = {}; // Notice change here
+        var keys = Object.keys(localStorage);
+        //console.log(keys);
+        var i = keys.length;
+
+          while ( i-- ) {
+            archive[ keys[i] ] = localStorage.getItem( keys[i] );
+            that.user[i]=JSON.parse(archive[keys[i]]);
+           
+            //console.log("Nikhil",that.user[i].name);
+          }
+
+      }
+      console.log(that.user);
+
     var form = document.getElementById("form");
     form.addEventListener('submit', function(e){
+    
       e.preventDefault();
       //console.log(e);
       var inputValue =(<HTMLInputElement>document.getElementById("mySearch")).value;
 
      var gitName=inputValue.split(" ").join('');
-     // console.log(gitName);
-     // alert(gitName);
+    //  console.log(gitName);
+    //   alert(gitName);
+
+    fetch("https://api.github.com/users/"+gitName)
+    .then((result)=>result.json())
+    .then(data=>{
+      if(!data.message){
+        data.message="Successfully !! found the user!"
+      }
+      else{
+        data.login=inputValue,
+        data.html_url="Invalid !!"
+        //data.message="Successfully !! found the user!"
+      }
+      var userModel ={
+         "name": data.login,
+         "url": data.html_url,
+         "result":data.message
+      };
+
+      const userObj = JSON.stringify(userModel);
+      // if(!localStorage.getItem(data.login)){
+        localStorage.setItem(data.login, userObj);
+      // }
+      
+      
+      //document.getElementById("link").value
+      //console.log("Nikhil",userModel);
+    })
 
     })
+   
   }
- 
+
 
    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {AppService} from './app.service';
+// import {app}
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,HttpClientModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
